4731ee9c1bfe72088e1b0b93e6d19efd4811dc67
bhuwan36 <163036314+bhuwan36@users.noreply.github.com>
2024-03-12 03:35:15 +0530

    changed

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,5 @@
-
-import React, { useState } from "react";
-import Modal from "react-modal";
-import RegisterForm from "./RegisterForm";
+import React from "react";
+import NavBar from "../Components/NavBar";
 import Hero from "../Components/Hero";
 import Info from "../Components/Info";
 import About from "../Components/About";
@@ -9,31 +7,11 @@ import BookAppointment from "../Components/BookAppointment";
 import Reviews from "../Components/Reviews";
 import Doctors from "../Components/Doctors";
 import Footer from "../Components/Footer";
-import Navbar from "../Components/NavBar";
 
 function Home() {
-  const [showRegisterModal, setShowRegisterModal] = useState(false);
-
-  const openRegisterModal = () => {
-    setShowRegisterModal(true);
-  };
-
-  const closeRegisterModal = () => {
-    setShowRegisterModal(false);
-  };
   return (
     <div className="home-section">
-      <button onClick={openRegisterModal}>Register</button>
-      <Modal
-        isOpen={showRegisterModal}
-        onRequestClose={closeRegisterModal}
-        contentLabel="Register Form"
-        className="register-modal"
-        overlayClassName="register-overlay"
-      >
-        <RegisterForm onClose={closeRegisterModal} />
-      </Modal>
-      <Navbar />
+      <NavBar />
       <Hero />
       <Info />
       <About />
