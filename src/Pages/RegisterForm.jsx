874fdd0fb799beacd002f06760f751a7cdb46139
bhuwan36 <163036314+bhuwan36@users.noreply.github.com>
2024-03-12 21:33:10 +0530

    form updated

--- src/Pages/RegisterForm.jsx
+++ src/Pages/RegisterForm.jsx
@@ -60,9 +60,12 @@ const RegisterForm = ({ onClose }) => {
                     <input type="text" name="phone" placeholder="Phone" value={formData.phone} onChange={handleChange} />
                     <input type="date" name="dob" value={formData.dob} onChange={handleChange} />
                     <select name="hq" value={formData.hq} onChange={handleChange}>
-                        <option value="">Select HQ</option>
-                        <option value="hq1">HQ 1</option>
-                        <option value="hq2">HQ 2</option>
+                        <option value="">Select Highest Qualification</option>
+                        <option value="hq1">Matriculation</option>
+                        <option value="hq2">Intermediate</option>
+                        <option value="hq2">Graduation</option>
+                        <option value="hq2">Post Graduation</option>
+                        <option value="hq2">Diploma</option>
                         {/* Add more options as needed */}
                     </select>
                     <select name="gender" value={formData.gender} onChange={handleChange}>
