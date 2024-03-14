d5e3f67d7ad02c0be782c242c716f3850acc4272
bhuwan36 <163036314+bhuwan36@users.noreply.github.com>
2024-03-14 12:28:43 +0530

    gallery updated

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import ReactSimplyCarousel from 'react-simply-carousel';
 import '../Styles/Gallery.css';
+import Image0 from '../Assets/gallery/image0.jpg';
+import Image0_1 from '../Assets/gallery/image0.1.jpg';
 import Image1 from '../Assets/gallery/image1.jpg';
 import Image2 from '../Assets/gallery/image2.jpg';
 import Image3 from '../Assets/gallery/image3.jpg';
@@ -69,6 +71,8 @@ function Gallery() {
                 speed={400}
                 easing="linear"
             >
+                <img style={{ border: '2px solid black', borderRadius: '10px', width: '18rem', height: '50vh', objectFit: 'contain', marginTop: '20px', marginLeft: '5px', }} src={Image0} />
+                <img style={{ border: '2px solid black', borderRadius: '10px', width: '18rem', height: '50vh', objectFit: 'contain', marginTop: '20px', marginLeft: '5px', }} src={Image0_1} />
                 <img style={{ border: '2px solid black', borderRadius: '10px', width: '18rem', height: '50vh', objectFit: 'contain', marginTop: '20px', marginLeft: '5px', }} src={Image1} />
                 <img style={{ border: '2px solid black', borderRadius: '10px', width: '18rem', height: '50vh', objectFit: 'contain', marginTop: '20px', marginLeft: '5px', }} src={Image2} />
                 <img style={{ border: '2px solid black', borderRadius: '10px', width: '18rem', height: '50vh', objectFit: 'contain', marginTop: '20px', marginLeft: '5px', }} src={Image3} />
